2a38aad1afaf76aa6812afe056e61047ec7a453a
Marko Gargenta <marko@marakana.com>
2011-12-21 04:18:52 -0800

    Introduced QWifiApplication to manage QWifiManager.

--- QWifiClient/src/qcom/wificlient/QWifiClientActivity.java
+++ QWifiClient/src/qcom/wificlient/QWifiClientActivity.java
@@ -8,24 +8,17 @@ import android.view.View;
 
 public class QWifiClientActivity extends Activity {
     static final String TAG = "QWifi";
-    QWifiManager wifi;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        if (wifi == null)
-            wifi = new QWifiManager(this);
-    }
-
-    @Override
-    protected void onDestroy() {
-        wifi.close();
-        super.onDestroy();
     }
 
     /** Called when button in clicked. */
     public void onClick(View v) {
+        QWifiManager wifi = ((QWifiApplication) getApplication()).wifi;
+        
         wifi.enableWifi();
         Log.d(TAG, "onClick got: " + wifi.connect("MySSID"));
     }
--- QWifiCommon/src/qcom/wificommon/IQWifiService.aidl
+++ QWifiCommon/src/qcom/wificommon/IQWifiService.aidl
@@ -3,4 +3,5 @@ package qcom.wificommon;
 interface IQWifiService {
 	int connect(String ssid);
 	int enableWifi();
+	QWifiInfo getConnectionInfo();
 }
