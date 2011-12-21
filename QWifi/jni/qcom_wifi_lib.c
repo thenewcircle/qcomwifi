d4461773ee3ef568b3ed1d9be0a7757646adfda5
Marko Gargenta <marko@marakana.com>
2011-12-21 04:37:28 -0800

    Removed LOGD

--- QWifi/jni/qcom_wifi_lib.c
+++ QWifi/jni/qcom_wifi_lib.c
@@ -1,14 +1,11 @@
 #define LOG_TAG "QcomWifi"
 #include <stdio.h>
-#include <android/log.h>
 
 int enableWifi() {
-	LOGD("enableWifi");
 	return 1;
 }
 
 int connect( char *ssid ) {
 	printf("%s", ssid);
-	LOGD("connect");
 	return 1;
 }
