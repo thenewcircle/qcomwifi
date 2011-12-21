800fcc85bd5303ffd0d68c7316eee694b5194949
Marko Gargenta <marko@marakana.com>
2011-12-21 01:02:35 -0800

    Added

---
+++ QWifiClient/src/qcom/wificlient/QWifiClientActivity.java
@@ -0,0 +1,13 @@
+package qcom.wificlient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QWifiClientActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
